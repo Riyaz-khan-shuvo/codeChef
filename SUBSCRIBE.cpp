7367883f4848c710cb6a3e8979beaadd986dda9d
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-11-26 11:15:22 +0600

    subscribe done

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, a;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &a);
+        if (a <= 30)
+        {
+            printf("NO\n");
+        }
+        else
+        {
+            printf("YES\n");
+        }
+    }
+    return 0;
+}
