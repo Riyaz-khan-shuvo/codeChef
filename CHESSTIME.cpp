e045f0648cb1562074a4d88b505f82dc12b391f7
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-11-26 12:00:08 +0600

    chesstime done

---
+++ CHESSTIME.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, a;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &a);
+        printf("%d\n", (a * 60 / 20));
+    }
+
+    return 0;
+}
