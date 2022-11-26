dc9006ca71d562b28e79c935bd936189df8c92a2
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-11-26 11:11:25 +0600

    credes done

---
+++ CREDS.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, a, b, c;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        printf("%d\n", ((a * 4) + (b * 2) + (c * 0)));
+    }
+
+    return 0;
+}
