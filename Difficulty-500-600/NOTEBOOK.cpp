b07234ce594c66b0a7d25b9d95e837435440c551
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-11-28 18:52:57 +0600

    notebook done

---
+++ Difficulty-500-600/NOTEBOOK.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, a;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &a);
+        printf("%d\n", (a * 1000) / 100);
+    }
+
+    return 0;
+}
