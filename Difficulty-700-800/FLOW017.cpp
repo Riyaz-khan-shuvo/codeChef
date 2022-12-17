4161b2f965006229e62ad82791f4836e85984867
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-12-17 22:10:07 +0600

    flow017 has not done

---
+++ Difficulty-700-800/FLOW017.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c, d, t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        if (a >= b && a >= c)
+        {
+            if (b >= c)
+            {
+                d = b;
+            }
+            else
+            {
+                d = c;
+            }
+        }
+        else if (b >= a && b >= c)
+        {
+            if (a >= c)
+            {
+                d = a;
+            }
+            else
+            {
+                d = c;
+            }
+        }
+        else if (a >= b)
+        {
+            if (a < c)
+            {
+                d = a;
+            }
+
+            else
+            {
+                d = b;
+            }
+        }
+
+        printf("%d\n", d);
+    }
+    return 0;
+}
