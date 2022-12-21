6f3bc52e064bf0c77e9d399caa0fad2095d389c4
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-12-21 18:14:03 +0600

    blackcel has done

---
+++ Difficulty-700-800/BLACKCEL.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n % 2 == 0)
+    {
+        printf("%d\n", (n * n) / 2);
+    }
+
+    return 0;
+}
