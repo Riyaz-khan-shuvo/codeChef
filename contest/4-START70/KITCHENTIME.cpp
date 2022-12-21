c7f580c27ae72edc0afa7a07f0500def07933381
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-12-21 20:36:10 +0600

    kitchentime has donew

---
+++ contest/4-START70/KITCHENTIME.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, x, y;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d %d", &x, &y);
+        printf("%d\n", y - x);
+    }
+
+    return 0;
+}
