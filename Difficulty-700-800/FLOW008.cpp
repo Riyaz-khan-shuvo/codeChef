b90e2dc4ed22db4cea86c9a04269d25cce81f9b9
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-12-25 10:19:24 +0600

    flow008 has done

---
+++ Difficulty-700-800/FLOW008.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        n < 10 ? printf("Thanks for helping Chef!\n") : printf("%d\n", -1);
+    }
+
+    return 0;
+}
